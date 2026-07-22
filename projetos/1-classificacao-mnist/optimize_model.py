633e034c392f03f71910a32cdebd5ab25d7e7a99
Pedro <pedrohenriquefa97@gmail.com>
2026-07-21 22:15:40 -0300

    Entrega do desafio técnico - Pedro Henrique Ferreira Amorim da Silva

--- projetos/1-classificacao-mnist/optimize_model.py
+++ projetos/1-classificacao-mnist/optimize_model.py
@@ -13,3 +13,26 @@ import os
 # ---------------------------------------------------------------------------
 
 # insira seu código aqui
+
+def main():
+    # Garanti que os caminhos funcionem independente de onde o script for chamado
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    h5_path = os.path.join(script_dir, "model.h5")
+    tflite_path = os.path.join(script_dir, "model.tflite")
+
+    # Carrega o modelo treinado em "model.h5"
+    model = tf.keras.models.load_model(h5_path)
+
+    # Converte para TensorFlow Lite usando tf.lite.TFLiteConverter
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+
+    # Aplica uma técnica de otimização
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+    tflite_model = converter.convert()
+
+    # Salva o resultado como "model.tflite"
+    with open(tflite_path, "wb") as f:
+        f.write(tflite_model)
+
+if __name__ == "__main__":
+    main()
